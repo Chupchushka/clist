25861c65d7fd473decc59730e453001924dea42e
Chupchushka <rabcevskijvana455@gmail.com>
2025-09-12 19:16:51 +0200

    feature: now we can mark tasks done or not

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,12 @@ int main() {
   db_service->openDb("clist-db.db");
   todo_service -> createTable();
   todo_service->addTask("Example task");
+  todo_service->addTask("Example task2");
+  todo_service->addTask("Example task3");
+
+  todo_service->markDone(true, 2);
   todo_service->removeTask(1);
+  db_service->readDataStmt();
   db_service->closeDB();
 
   return 0;
